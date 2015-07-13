e7fedac4d39864e2a7503bee0146ad266200ee53
Ashmeet <hackercompany@gmail.com>
2015-07-13 13:31:33 +0530

    removed xception

--- django_project/api/dealsApi.py
+++ django_project/api/dealsApi.py
@@ -120,7 +120,6 @@ def get_deals(request,user, category, typ):
                 try:
                     price = int(float(re.sub("[^\d+\.]","",price).strip(".")))
                 except:
-                    raise
                     pass
                 merdata['price'] = price
                 merdata.update({"open":op})
